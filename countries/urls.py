de5dbaf858cd2be82f2b9c2abacba1c5d533f724
it1913 <66723368+it1913@users.noreply.github.com>
2022-06-05 14:12:41 +0200

    3. Template

--- countries/urls.py
+++ countries/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('list/', views.CountryListView.as_view(), name='list'),
+    path('detail/<int:pk>/', views.CountryDetailView.as_view(), name='country_detail'),
+]
 
 
--- countries/views.py
+++ countries/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from countries.models import Country,Town,Region,Attachment
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+def index(request):
+     num_countries = Country.objects.all().count()
+     countries = Country.objects.order_by('-cName')[:3]
+     context = {
+          'num_countries': num_countries,
+          'countries': countries
+     }
+     return render(request, 'index.html', context=context)
+
+
+class CountryListView(ListView):
+     model = Country
+     context_object_name = 'country_list'
+     template_name = 'country/list.html'
+
+
+class CountryDetailView(DetailView):
+     model = Country
+     context_object_name = 'country_detail'
+     template_name = 'country/detail.html'
+
+
